1a6253a93c1e5f6bc63259cdb7c5c03befd0dbca
Victor Andrade <victor64gaming@gmail.com>
2025-01-17 21:54:07 -0300

    service to consume the provider

---
+++ src/main/java/com/uber/email_service/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.uber.email_service.application;
+
+import com.uber.email_service.adapters.EmailSenderGateway;
+import com.uber.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
