cf7102e5ec4f35e2b307d7fa8aea81211f15a5cf
Victor Andrade <victor64gaming@gmail.com>
2025-01-17 21:51:23 -0300

    initial setup

---
+++ src/test/java/com/uber/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
